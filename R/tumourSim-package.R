4bdca93f6366ccdcd349ecf87cae368df14101d3
Calum Regan <c_regan1888@outlook.com>
2026-04-04 21:31:52 +0100

    updated package requirements

--- R/tumourSim-package.R
+++ R/tumourSim-package.R
@@ -5,4 +5,8 @@
 #' @importFrom survival survfit
 #' @importFrom VineCopula BiCopSelect
 #' @importFrom VineCopula BiCopCDF
+#' @importFrom ggplot2 ggplot aes geom_ribbon geom_line geom_point
+#'   scale_y_continuous scale_x_continuous labs theme_minimal theme
+#'   element_text element_line element_blank
+#' @importFrom patchwork plot_annotation wrap_plots
 "_PACKAGE"
